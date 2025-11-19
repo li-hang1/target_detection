8aa04f0e2bb983f327d0d54ff7f21f6b4d0b31e3
li-hang1 <lh2038672150@gmail.com>
2025-11-19 18:25:15 +0800

    update

--- utils/generate_anchors.py
+++ utils/generate_anchors.py
@@ -1,6 +1,7 @@
 import torch
+import numpy as np
 
-def generate_anchors(feature_map_size, strides, image_size, scales=torch.tensor([64, 128, 256]), ratios=torch.tensor([0.5, 1, 2]), device='cpu'):
+def generate_anchors(feature_map_size, strides, image_size, scales=(64, 128, 256), ratios=(0.5, 1, 2), device='cpu'):
     H, W = feature_map_size
     stride_H, stride_W = strides
     anchors = []
@@ -10,12 +11,16 @@ def generate_anchors(feature_map_size, strides, image_size, scales=torch.tensor(
             ctr_y = i * stride_H + stride_H / 2
             for scale in scales:
                 for ratio in ratios:
-                    w = scale * torch.sqrt(ratio)
-                    h = scale / torch.sqrt(ratio)
+                    w = scale * np.sqrt(ratio)
+                    h = scale / np.sqrt(ratio)
                     x1 = ctr_x - w / 2
                     y1 = ctr_y - h / 2
                     x2 = ctr_x + w / 2
                     y2 = ctr_y + h / 2
-                    anchors.append(torch.stack([x1, y1, x2, y2]))
+                    anchors.append([x1, y1, x2, y2])
+    anchors = torch.tensor(anchors, dtype=torch.float32, device=device)
+    anchors = clip_anchors_to_image(anchors, image_size)
+    return anchors
+
 
 
